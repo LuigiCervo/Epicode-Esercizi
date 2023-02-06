ea4804d907cd1388c2aa7cd73844f970aa640337
W0lfBl4d3 <120737722+W0lfBl4d3@users.noreply.github.com>
2023-02-06 16:24:30 +0100

    Add files via upload

---
+++ Epicode/Esercizi-Epicode/Week2/EsercizioG1/assets/js/primo.js
@@ -0,0 +1,4 @@
+document.getElementById('corpo').innerHTML = '<div id="container2"><h1>Iniziamo la nostra pratica</h1><br><p>Output scritto da Javascript</p></div>'
+
+window.alert('Questo è un alert fatto con Javascript')
+alert('Forza Napoli')
---
+++ Epicode/Esercizi-Epicode/Week2/EsercizioG1/assets/js/secondo.js
@@ -0,0 +1,15 @@
+var Prima_Variabile = 'pantera';
+document.write( Prima_Variabile + ' scritto da Javascript ' );
+
+var Seconda_variabile = 'Fenicottero';
+
+console.log(Seconda_variabile);
+
+var terza_variabile = 'Lupo';
+
+console.log(terza_variabile);
+
+var quartaVariabile = 'Coccodrillo';
+
+console.log(quartaVariabile);
+
